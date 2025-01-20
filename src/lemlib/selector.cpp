d84822b44b8c61f07c3556a8153991b5aab49107
Haoran Fang <ifh_fang.h@outlook.com>
2025-01-20 14:53:49 +1100

    V.EE1.111 - Added hours wasted counter for selector code

--- src/lemlib/selector.cpp
+++ src/lemlib/selector.cpp
@@ -520,3 +520,19 @@ int customUpdate() {
 
 
 } // namespace lemlib::selector
+
+//----------------------------------------------------------------//
+//                                                                //
+// Dear Programmer(s)/Manager(s),                                 //
+//                                                                //
+// When I wrote this code, only God and I knew how it worked.     //
+// Now, only God knows it...                                      //
+//                                                                //
+// Therefore, if you are trying to optimize this routine          //
+// and it fails (most surely), please increase this counter       //
+// as a warning for the next person:                              //
+//                                                                //
+// total_hours_waster_here = 5                                    //
+//                                                                //
+//----------------------------------------------------------------//
+
